f7cd7064ed6ccdd7bba5c5fcf4e78e7ab06d9a13
huoxy <sykp241095@gmail.com>
2013-06-02 17:40:33 +0800

    add graphite-observer.py

---
+++ graphite-observer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+from bottle import run, debug
+import config
+
+app_dir = os.path.dirname(__file__)
+if app_dir:
+    os.chdir(app_dir)
+
+debug(True)
+run('app', host = config.listen_host, port = config.listen_port)
