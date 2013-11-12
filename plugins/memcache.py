36ab7b819e1f5bf5a1d38b88dab557ab20a156c4
sykp241095 <sykp241095@gmail.com>
2013-11-12 12:24:11 +0800

    use it

--- plugins/memcache.py
+++ plugins/memcache.py
@@ -1,7 +1,12 @@
 targets = [
     {
-        'reg' : 'servers.thorin.*memcache.*hit_rate',
+        'reg' : 'servers.thorin.*memcache.shire-mc.*hit_rate',
         'max' : 100,
         'min' : 90,
+    },
+    {
+        'reg' : 'servers.thorin.*memcache.shire-bak-mc.*bytes_read',
+        'max' : 1024 * 1024,
+        'min' : 0,
     },
 ]
