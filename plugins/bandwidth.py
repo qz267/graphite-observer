d0bd4bcf6a58656ab5d5e6aeaf8624e15d89bbf8
sykp241095 <sykp241095@gmail.com>
2013-11-12 12:32:04 +0800

    add bandwidth

---
+++ plugins/bandwidth.py
@@ -0,0 +1,12 @@
+targets = [
+    {
+        'reg' : 'servers.thorin.bandwidth.in',
+        'max' : 200 * 1024 * 1024,
+        'min' : 0,
+    },
+    {
+        'reg' : 'servers.thorin.bandwidth.in',
+        'max' : 700 * 1024 * 1024,
+        'min' : 0,
+    },
+]
